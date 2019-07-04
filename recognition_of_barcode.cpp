850f227831d1e8e77d167c4a118fa2e4413cf09e
cui0409 <51311442+cui0409@users.noreply.github.com>
2019-07-04 10:51:55 +0800

    Add files via upload

--- recognition_of_barcode.cpp
+++ recognition_of_barcode.cpp
@@ -181,22 +181,10 @@ Mat getRect(Mat image, Mat simage, bool show)
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int       nCmdShow)
 {
 	VideoCapture capture(1);
-	Mat camera;
-
 	if (!capture.isOpened())
 	{
 		return -1;
 	}
-	//读取一帧图像
-
-
-	//double rate = capture.get(CAP_PROP_FPS); // 获取视频帧率
-	//capture.set(CAP_PROP_POS_FRAMES, rate);//读取第 帧,第一秒
-	//capture >> camera;
-	capture.read(camera);
-	imshow("sss", camera);
-
-
 
 	string time = get_time();
 
@@ -206,77 +194,73 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	sprintf_s(img_chenggong, "%s%s%s", "F:\\测试条码用\\成功\\", time.c_str(), ".jpg");
 	sprintf_s(img_shibai, "%s%s%s", "F:\\测试条码用\\失败\\", time.c_str(), ".jpg");
 
-
-	//Mat camera = imread("F:\\2019-6-29-10-10-12-667.jpg");
-	//imshow("原图", camera);
-	//Mat image;
-	//image = getGray(srcimage, true);//获取灰度图
-	//image = getGass(image, true);//高斯平滑滤波
-	//image = getSobel(image, true);//Sobel x—y梯度差
-	//image = getBlur(image, true);//均值滤波除高频噪声
-	//image = getThold(image, true);//二值化
-	//image = getBys(image, true);//闭运算
-	//image = getErode(image, true);//腐蚀
-	//image = getDilate(image, true);//膨胀
-
-	//image = getRect(image, srcimage, true);//获取ROI
-	//imshow("处理结果图", image);
-
 	ImageScanner scanner;
 	scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
-
-	Mat imageGray;
-	cvtColor(camera, imageGray, CV_RGB2GRAY);
-
-	int width = imageGray.cols;
-	int height = imageGray.rows;
-	uchar *raw = (uchar *)imageGray.data;//raw中存放的是图像的地址
-	Image imageZbar(width, height, "Y800", raw, width * height);
-
-	//扫描条码  
-	scanner.scan(imageZbar);
 
 	//2个码
 	string str_barcode_chip;//CHIP ID码
 	string str_barcode_sn;//SN码
 
-	Image::SymbolIterator symbol = imageZbar.symbol_begin();
-	if (imageZbar.symbol_begin() == imageZbar.symbol_end())
+	double rate = capture.get(CAP_PROP_FPS); // 获取视频帧率
+
+	Mat camera;
+	for (int i = 0; i < rate * 3; ++i)//3秒循环识别
 	{
-		MessageBox(NULL, TEXT("识别失败"), TEXT("条码结果"), MB_DEFBUTTON1 | MB_DEFBUTTON2);
-		imwrite(img_shibai, camera);
-		return -1;
-	}
-	else
-	{
+		capture.set(CAP_PROP_POS_FRAMES, i);//读取第i帧
+		capture >> camera;
+
+		Mat imageGray;
+
+		cvtColor(camera, imageGray, CV_RGB2GRAY);
+
+		int width = imageGray.cols;
+		int height = imageGray.rows;
+		uchar *raw = (uchar *)imageGray.data;//raw中存放的是图像的地址
+		
+		Image imageZbar(width, height, "Y800", raw, width * height);
+
+		//扫描条码  
+		scanner.scan(imageZbar);
+
+		Image::SymbolIterator symbol = imageZbar.symbol_begin();
+
 		str_barcode_chip = symbol->get_data();
-
 		++symbol;
-
 		str_barcode_sn = symbol->get_data();
+
+		CString chip, sn;
+
+		//只要sn和chip码全部扫出来，即退出循环保存成功结果，否则走完循环还扫不出来就确实是扫不出来
+		if((!str_barcode_chip.empty()) && (!str_barcode_sn.empty()))
+		{
+			chip = str_barcode_chip.c_str();
+			sn = str_barcode_sn.c_str();
+			imshow("sss", camera);
+			MessageBox(NULL, TEXT("SN:") + sn + TEXT("\nCHIP ID:") + chip, TEXT("条码结果"), MB_DEFBUTTON1 | MB_DEFBUTTON2);
+			imwrite(img_chenggong, camera);
+
+			//扫码成功，将得到的CHIP ID码和SN码保存到csv文件中
+			char barcode_file[100];
+			sprintf_s(barcode_file, "%s%s%s", "F:\\条码保存\\barcode_csv\\barcode", time.c_str(), ".csv");
+			ofstream outFile;
+			outFile.open(barcode_file, ios::out);
+
+
+			outFile << "chip:" << str_barcode_chip << "," << "sn:" << str_barcode_sn << endl;
+			outFile.close();
+
+			// 读文件  
+			ifstream inFile(barcode_file, ios::in);
+
+			return 0;//扫码成功就退出
+		}
+
 	}
 
-	CString chip, sn;
-	chip = str_barcode_chip.c_str();
-	sn = str_barcode_sn.c_str();
+	//循环识别还是没有结果，那就是失败了
+	MessageBox(NULL, TEXT("识别失败"), TEXT("条码结果"), MB_DEFBUTTON1 | MB_DEFBUTTON2);
+	imwrite(img_shibai, camera);
 
-	MessageBox(NULL, TEXT("SN:") + sn + TEXT("\nCHIP ID:") + chip, TEXT("条码结果"), MB_DEFBUTTON1 | MB_DEFBUTTON2);
-	imwrite(img_chenggong, camera);
-
-	//扫码成功，将得到的CHIP ID码和SN码保存到csv文件中
-	char barcode_file[100];
-	sprintf_s(barcode_file, "%s%s%s", "F:\\条码保存\\barcode_csv\\barcode", time.c_str(), ".csv");
-	ofstream outFile;
-	outFile.open(barcode_file, ios::out);
-
-
-	outFile << "chip:" << str_barcode_chip << "," << "sn:" << str_barcode_sn << endl;
-	outFile.close();
-
-	// 读文件  
-	ifstream inFile(barcode_file, ios::in);
-
-	waitKey();
-
+	return 0;
 }
 
